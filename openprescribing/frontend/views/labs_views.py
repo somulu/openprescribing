7e00ec476ff4fbf048c45bb4e55e4d3003a1677f
Seb Bacon <seb.bacon@gmail.com>
2017-01-24 12:08:32 +0000

    Imrove histogram layout
    * Better styling
    * Axes
    * Consistent bin sizes across brands

--- openprescribing/frontend/views/labs_views.py
+++ openprescribing/frontend/views/labs_views.py
@@ -1,9 +1,14 @@
+import base64
 from StringIO import StringIO
+
+import matplotlib
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
-import base64
-import matplotlib
-import pandas as pd
+
 
 
 @cache_page(0)
@@ -11,30 +16,43 @@ def data_for_equivalents(request, code, date):
     matplotlib.use('Agg')
     import matplotlib.pyplot as plt
     hide_generic = request.GET.get('hide_generic', False)
-    url = 'http://staging.openprescribing.net/api/1.0/bnf_code/data_for_equivalents'
-    df = pd.read_json(
-        url + '?q=%s&date=%s&format=json' % (code, date),
-        orient='records')
+    url = request.build_absolute_uri(
+        "/api/1.0/bnf_code/data_for_equivalents"
+        "?q=%s&date=%s&format=json" % (code, date))
+    df = pd.read_json(url, orient='records')
     df['ppq'] = df['actual_cost'] / df['quantity']
-    fig, ax = plt.subplots()
-    plt.figure(figsize=(14, 8))
     generic_name = ''
     if len(df) > 0:
-        for name in reversed(df.groupby('presentation_name').groups.keys()):
+        plt.figure(figsize=(12, 8))
+        generic_name = ""
+        data = []
+        hist, bin_edges = np.histogram(df['ppq'])
+        ordered = df.groupby('presentation_name')['ppq'].aggregate({'mean_ppq': 'mean'}).sort_values('mean_ppq').index
+        for name in ordered:
             current = df[df.presentation_name == name]
-            if hide_generic and current.iloc[0].presentation_code[9:11] == 'AA':
+            if current.iloc[0].presentation_code[9:11] == 'AA':
                 generic_name = current.loc[0].presentation_name
-                continue
-            plt.hist(current['ppq'],
-                     label=name,
-                     histtype='stepfilled',
-                     edgecolor='none')
-            plt.legend(loc='upper right')
-        plt.title("ppq for various brands of %s" % generic_name)
-        canvas = plt.gca().figure.canvas
-        canvas.draw()
+                if hide_generic:
+                    continue
+            data.append(
+                plt.hist(
+                    current['ppq'],
+                    bins=len(bin_edges),
+                    range=(min(bin_edges),max(bin_edges)),
+                    label=name,
+                    histtype='stepfilled',
+                    normed=True,
+                    edgecolor='none'))
+
+        plt.legend(bbox_to_anchor=(1,1), loc=2)
+        plt.ylabel('probability density')
+        plt.xlabel('price per quantity')
+        if hide_generic:
+            plt.title("Price per quantity for brands")
+        else:
+            plt.title("Price per quantity for brands and generics")
         ob = StringIO()
-        canvas.print_png(ob)
+        plt.savefig(ob, dpi=75, bbox_inches='tight')
         ob.seek(0)
         imgdata = base64.b64encode(ob.read())
     else:
